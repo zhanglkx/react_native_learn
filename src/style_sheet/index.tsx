858b4f1c913790c64c1b6ef2db903598267dd3cf
zhanglike <zhanglkx@foxmail.com>
2024-01-16 23:31:11 +0800

    🌟feat: (syle-sheet) 👉数组形式 当多个样式有属性重叠，后面的样式优先级高

--- src/style_sheet/index.tsx
+++ src/style_sheet/index.tsx
@@ -7,8 +7,12 @@ export default function index() {
     <View>
       {/* 对象形式的 style sheet */}
       <Text style={{fontSize: 20, color: 'red', padding: 100}}>index</Text>
-      {/* 数组形式 */}
-      <Text style={[{fontSize: 20, color: 'red', padding: 100}]}>index</Text>
+
+      {/* 数组形式 当多个样式有属性重叠，后面的样式优先级高 */}
+      <Text
+        style={[{fontSize: 20, color: 'red', padding: 100}, {color: 'pink'}]}>
+        index
+      </Text>
     </View>
   );
 }
