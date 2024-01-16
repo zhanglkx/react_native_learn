6da8eb18cfcccdf62d1efb3e9c03089ab1db4fa2
zhanglike <zhanglkx@foxmail.com>
2024-01-16 23:58:15 +0800

    🌟feat: (flex) 👉 height

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 import {View, Text} from 'react-native';
 import React from 'react';
-import StyleSheet from './src/style_sheet/index';
+// import StyleSheet from './src/style_sheet/index';
+import FlexDirection from './src/FlexBox/FlexDirection';
 
 export default function App() {
   return (
     <View>
-      <Text style={{paddingTop: 100}}> textInComponent </Text>
-      <StyleSheet />
+      <Text style={{paddingTop: 100}}> textInCo1mponent </Text>
+      <FlexDirection />
     </View>
   );
 }
