ff89259f2a93913d263e477594baa11ecc3aa09c
zhanglk <zhanglkx@foxmail.com>
2024-01-17 14:46:57 +0800

    🌟 feat:(RN-Navigator)  👉   返回主页

--- src/Navigator/DetailsScreen/DetailsScreen.tsx
+++ src/Navigator/DetailsScreen/DetailsScreen.tsx
@@ -11,6 +11,10 @@ function DetailsScreen({navigation}) {
       />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
