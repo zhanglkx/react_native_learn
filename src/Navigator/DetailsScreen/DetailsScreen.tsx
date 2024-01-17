27cbbd22c79e8f0112d5981a3ccbf0e4abf3af3a
zhanglk <zhanglkx@foxmail.com>
2024-01-17 14:26:51 +0800

    🌟 feat:(RN-Navigator)  👉   返回主页、前进、后退

--- src/Navigator/DetailsScreen/DetailsScreen.tsx
+++ src/Navigator/DetailsScreen/DetailsScreen.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {Button, View, Text} from 'react-native';
-export default function DetailsScreen({navigation}) {
+function DetailsScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
@@ -9,6 +9,10 @@ export default function DetailsScreen({navigation}) {
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
+
+export default DetailsScreen;
