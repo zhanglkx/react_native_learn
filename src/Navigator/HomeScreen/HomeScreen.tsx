f028608f920b0be86c5f38175752d9712c275045
zhanglk <zhanglkx@foxmail.com>
2024-01-17 14:49:02 +0800

    🌟 feat:(RN-Navigator)  👉   导航传参

--- src/Navigator/HomeScreen/HomeScreen.tsx
+++ src/Navigator/HomeScreen/HomeScreen.tsx
@@ -2,15 +2,22 @@
 import * as React from 'react';
 import {Button, View, Text} from 'react-native';
 
-export default function HomeScreen({navigation}) {
-  console.log(navigation);
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
       />
     </View>
   );
 }
+
+export default HomeScreen;
