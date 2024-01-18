bb3f63150f185e554a8126851860db8ebec39f4d
zhanglk <zhanglkx@foxmail.com>
2024-01-18 13:43:23 +0800

    🌟 feat:(RN-Navigator) 👉  自定义导航栏样式

--- App.jsx
+++ App.jsx
@@ -1,13 +1,36 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/no-unstable-nested-components */
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, Button, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 function HomeScreen() {
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function LogoTitle() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        margin: 0,
+        padding: 0,
+      }}>
+      <Button title="左侧导航栏" style={styles.lefyStyle} />
+
+      <Image
+        style={styles.centerStyle}
+        source={require('./src/assets/images/elk.png')}
+      />
+      <Button title="左侧导航栏" style={{fontSize: 20}} />
     </View>
   );
 }
@@ -17,21 +40,38 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-        }}>
+      <Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: 'My home'}}
+          options={{headerTitle: props => <LogoTitle {...props} />}}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  lefyStyle: {
+    textAlign: 'left',
+    fontSize: 20,
+    borderColor: 'red',
+    borderWidth: 1,
+    backgroundColor: 'red',
+  },
+  centerStyle: {
+    textAlign: 'left',
+    fontSize: 20,
+    borderColor: 'red',
+    borderWidth: 1,
+    width: 40,
+    height: 40,
+  },
+  rightStyle: {
+    textAlign: 'left',
+    fontSize: 20,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+});
 export default App;
