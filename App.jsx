e711ad16f40ceb8e300d1c541f07bc5c0366743c
zhanglk <zhanglkx@foxmail.com>
2024-01-18 16:46:03 +0800

    🌟 feat:(RN-Navigator) 👉   Drawer 导航 格式更改

--- App.jsx
+++ App.jsx
@@ -1,29 +1,29 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {Button, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 function EmptyScreen() {
   return <View />;
 }
 
-function Feed({ navigation }) {
+function Feed({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Feed Screen</Text>
       <Button title="Go to Root" onPress={() => navigation.navigate('Root')} />
       <Button
         title="Go to Root, Profile"
-        onPress={() => navigation.navigate('Root', { screen: 'Profile' })}
+        onPress={() => navigation.navigate('Root', {screen: 'Profile'})}
       />
     </View>
   );
 }
 
-function Home({ navigation }) {
+function Home({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button title="Go to Feed" onPress={() => navigation.navigate('Feed')} />
     </View>
@@ -35,7 +35,7 @@ const Stack = createNativeStackNavigator();
 
 function Root() {
   return (
-    <Drawer.Navigator >
+    <Drawer.Navigator>
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Profile" component={EmptyScreen} />
       <Stack.Screen name="Settings" component={EmptyScreen} />
