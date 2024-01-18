73716ba9f70f5e5b035ec33f0731e059b81bfce7
zhanglk <zhanglkx@foxmail.com>
2024-01-18 17:23:18 +0800

    🌟 feat:(RN-Navigator) 👉   导航通用样式，tabbar+Navigator

--- App.jsx
+++ App.jsx
@@ -3,66 +3,49 @@ import * as React from 'react';
 import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function SettingsScreen({route, navigation}) {
-  const {user} = route.params;
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Settings Screen</Text>
-      <Text>userParam: {JSON.stringify(user)}</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
-  );
-}
-
-function ProfileScreen({navigation}) {
+function Profile({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Profile Screen</Text>
-    </View>
-  );
-}
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
       <Button
-        title="Go to Settings"
-        onPress={() =>
-          navigation.navigate('Root', {
-            screen: 'Settings',
-            params: {user: 'jane'},
-          })
-        }
+        onPress={() => navigation.navigate('EditPost')}
+        title="Go to Edit Post"
       />
     </View>
   );
 }
 
-const Drawer = createDrawerNavigator();
+function EmptyScreen() {
+  return <View />;
+}
+
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function Root() {
+function Home() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Settings" component={EmptyScreen} />
+    </Tab.Navigator>
   );
 }
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Root">
-        <Drawer.Screen name="Root" component={Root} />
-        <Drawer.Screen name="Home" component={HomeScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="EditPost" component={EmptyScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
