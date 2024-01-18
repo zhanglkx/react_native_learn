ab90ed990f5a8f09b05d6f9a4fa278d353f03f15
zhanglk <zhanglkx@foxmail.com>
2024-01-18 18:01:29 +0800

    🌟 feat:(RN-Navigator) 👉   nesting navigators nest practice

--- App.jsx
+++ App.jsx
@@ -3,16 +3,26 @@ import * as React from 'react';
 import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function Profile({navigation}) {
+function Home({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Profile Screen</Text>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Help')} title="Go to Help" />
       <Button
-        onPress={() => navigation.navigate('EditPost')}
-        title="Go to Edit Post"
+        onPress={() => navigation.navigate('Profile')}
+        title="Go to Profile"
       />
+    </View>
+  );
+}
+
+function Help({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Help Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')} title="Go to Home" />
+      <Button onPress={() => navigation.navigate('Invite')} title="Invite" />
     </View>
   );
 }
@@ -21,28 +31,32 @@ function EmptyScreen() {
   return <View />;
 }
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function Home() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Settings" component={EmptyScreen} />
-    </Tab.Navigator>
-  );
-}
-
 function App() {
+  const isLoggedIn = true;
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="EditPost" component={EmptyScreen} />
+        {isLoggedIn ? (
+          // Screens for logged in users
+          <Stack.Group>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Profile" component={EmptyScreen} />
+          </Stack.Group>
+        ) : (
+          // Auth screens
+          <Stack.Group screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SignIn" component={EmptyScreen} />
+            <Stack.Screen name="SignUp" component={EmptyScreen} />
+          </Stack.Group>
+        )}
+        {/* Common modal screens */}
+        <Stack.Group screenOptions={{presentation: 'modal'}}>
+          <Stack.Screen name="Help" component={Help} />
+          <Stack.Screen name="Invite" component={EmptyScreen} />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
