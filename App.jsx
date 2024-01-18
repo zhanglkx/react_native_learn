40a98e3396efe731921bc9145bf9005f2c1c6407
zhanglk <zhanglkx@foxmail.com>
2024-01-18 19:22:49 +0800

    🌟 feat:(View) 👉 lificycle

--- App.jsx
+++ App.jsx
@@ -1,90 +1,85 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
+import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function Home({navigation}) {
+function SettingsScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      {/* 在不同的Stack.Group 之间跳转，实际上等同于 iOS 在不同的导航控制器之间跳转 */}
-      <Button onPress={() => navigation.navigate('Help')} title="Go to Help" />
+      <Text>Settings Screen</Text>
       <Button
-        onPress={() => navigation.navigate('Profile')}
         title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
       />
     </View>
   );
 }
 
-function Help({navigation}) {
+function ProfileScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Help Screen</Text>
-      <Button onPress={() => navigation.navigate('Home')} title="Go to Home" />
-      <Button onPress={() => navigation.navigate('Invite')} title="Invite" />
+      <Text>Profile Screen</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
     </View>
   );
 }
 
-function EmptyScreen() {
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.centeredText}>这是一段居中的文字。</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-function Nav() {
-  const isLoggedIn = true;
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+const SettingsStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {isLoggedIn ? (
-          // Screens for logged in users
-          <Stack.Group>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Profile" component={EmptyScreen} />
-          </Stack.Group>
-        ) : (
-          // Auth screens
-          <Stack.Group screenOptions={{headerShown: false}}>
-            <Stack.Screen name="SignIn" component={EmptyScreen} />
-            <Stack.Screen name="SignUp" component={EmptyScreen} />
-          </Stack.Group>
-        )}
-        {/* Common modal screens */}
-        <Stack.Group screenOptions={{presentation: 'modal'}}>
-          <Stack.Screen name="Help" component={Help} />
-          <Stack.Screen name="Invite" component={EmptyScreen} />
-        </Stack.Group>
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="First">
+          {() => (
+            <SettingsStack.Navigator>
+              <SettingsStack.Screen
+                name="Settings"
+                component={SettingsScreen}
+              />
+              <SettingsStack.Screen name="Profile" component={ProfileScreen} />
+            </SettingsStack.Navigator>
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Second">
+          {() => (
+            <HomeStack.Navigator>
+              <HomeStack.Screen name="Home" component={HomeScreen} />
+              <HomeStack.Screen name="Details" component={DetailsScreen} />
+            </HomeStack.Navigator>
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  const isLoggedIn = true;
-  return <Nav />;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  centeredText: {
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-    borderBlockColor: 'red',
-    borderWidth: 1,
-    color: 'red',
-    // height: 200,
-  },
-});
-export default App;
