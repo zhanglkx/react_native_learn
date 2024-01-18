b5467a110db3dbc540f4423eef3b4d8bba0dad18
zhanglk <zhanglkx@163.com>
2024-01-18 19:29:34 +0800

    🌟 feat:【navigation】👉   lifecycle effect

--- App.jsx
+++ App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-alert */
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {Button, View, Text} from 'react-native';
@@ -18,6 +20,16 @@ function SettingsScreen({navigation}) {
 }
 
 function ProfileScreen({navigation}) {
+  React.useEffect(
+    () => navigation.addListener('focus', () => alert('Screen was focused')),
+    [],
+  );
+
+  React.useEffect(
+    () => navigation.addListener('blur', () => alert('Screen was unfocused')),
+    [],
+  );
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Profile Screen</Text>
