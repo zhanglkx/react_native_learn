2825a814764ef05531babe1bb714563cf41684c0
zhanglike <zhanglkx@foxmail.com>
2024-01-22 06:53:00 +0800

    🌟feat: (提交内容) 👉add package

--- src/Navigator/AssetExample/AssetExample.jsx
+++ src/Navigator/AssetExample/AssetExample.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 import {Text, View, StyleSheet, Image} from 'react-native';
+import viewdemo from '@/pages/views/viewdemo';
 
 export default function AssetExample() {
   return (
     <View style={styles.container}>
+      <viewdemo />
       <Text style={styles.paragraph}>
         Local files and assets can be imported by dragging and dropping them
         into the editor
