2ae07d4f8ed117dc49bf106a1c8a179b5251b864
zhanglkx <zhanglkx@foxmail.com>
2024-01-22 08:37:49 +0800

    🌟 feat:【接口】👉 添加 fetch 接口

--- App.jsx
+++ App.jsx
@@ -1,107 +1,52 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
 
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
+// type Movie = {
+//   id: string;
+//   title: string;
+//   releaseYear: string;
+// };
 
-const RowGapAndColumnGap = () => {
-  const [rowGap, setRowGap] = useState(10);
-  const [columnGap, setColumnGap] = useState(10);
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      console.log(response);
+      const json = await response.json();
+      console.log(json);
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
-    <PreviewLayout
-      // 行间距
-      columnGap={columnGap}
-      handleColumnGapChange={setColumnGap}
-      // 列间距
-      rowGap={rowGap}
-      handleRowGapChange={setRowGap}>
-      <View style={[styles.box, styles.box1]} />
-      <View style={[styles.box, styles.box1]} />
-      <View style={[styles.box, styles.box1]} />
-      <View style={[styles.box, styles.box1]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-      <View style={[styles.box, {backgroundColor: getRandomColor()}]} />
-    </PreviewLayout>
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
+          )}
+        />
+      )}
+    </View>
   );
 };
 
-const PreviewLayout = ({
-  children,
-  handleColumnGapChange,
-  handleRowGapChange,
-  rowGap,
-  columnGap,
-}) => (
-  <View style={styles.previewContainer}>
-    <View style={styles.inputContainer}>
-      <View style={styles.itemsCenter}>
-        <Text>Row Gap</Text>
-        <TextInput
-          style={styles.input}
-          value={rowGap}
-          onChangeText={v => handleRowGapChange(Number(v))}
-        />
-      </View>
-      <View style={styles.itemsCenter}>
-        <Text>Column Gap</Text>
-        <TextInput
-          style={styles.input}
-          value={columnGap}
-          onChangeText={v => handleColumnGapChange(Number(v))}
-        />
-      </View>
-    </View>
-    <View style={[styles.container, {rowGap, columnGap}]}>{children}</View>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  itemsCenter: {alignItems: 'center'},
-  inputContainer: {
-    gap: 4,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  previewContainer: {padding: 10, flex: 1, marginTop: 90},
-  input: {
-    borderBottomWidth: 1,
-    paddingVertical: 3,
-    width: 50,
-    textAlign: 'center',
-  },
-  container: {
-    flex: 1,
-    marginTop: 8,
-    backgroundColor: 'aliceblue',
-    maxHeight: 400,
-    flexWrap: 'wrap',
-    alignContent: 'flex-start',
-  },
-  box: {
-    width: 50,
-    height: 80,
-  },
-  box1: {
-    backgroundColor: getRandomColor(),
-  },
-});
-
-export default RowGapAndColumnGap;
+export default App;
