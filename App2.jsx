1b832fcd3ef33487e80a8a44b8e56eae180d3d43
zhanglike <zhanglkx@foxmail.com>
2024-02-20 21:35:59 +0800

    导航器制作

--- App2.jsx
+++ App2.jsx
@@ -1,15 +1,34 @@
+// import React from 'react';
+// import * as eva from '@eva-design/eva';
+// import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+
+// const HomeScreen = () => (
+//   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//     <Text category="h1">HOME</Text>
+//   </Layout>
+// );
+
+// export default () => (
+//   <ApplicationProvider {...eva} theme={eva.light}>
+//     <HomeScreen />
+//   </ApplicationProvider>
+// );
+
 import React from 'react';
-import * as eva from '@eva-design/eva';
-import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+// import * as eva from '@eva-design/eva';
+import {ApplicationProvider} from '@ui-kitten/components'; // Provider
+import AppNavigator from './src/navigator/AppNavigator'; // 导航
+// import {default as theme} from './src/color/custom-theme.json'; // 如果没制作主题文件，可参考上一篇博文，或删除该段代码以及下方的theme变量使用
+import {SafeAreaView} from 'react-native-safe-area-context'; // 安全边界
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
+const App = () => {
+  return (
+    <ApplicationProvider>
+      <SafeAreaView style={{flex: 1}}>
+        <AppNavigator />
+      </SafeAreaView>
+    </ApplicationProvider>
+  );
+};
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+export default App;
---
+++ src/Navigator/TabNavigation.jsx
@@ -0,0 +1,99 @@
+// 用于编写底部导航栏的相关代码
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+import {MaterialIcons} from '@expo/vector-icons';
+
+// screens
+// import AccountScreen from "../screens/Account";
+
+const HomeScreen = props => {
+  return (
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home Screen</Text>
+    </Layout>
+  );
+};
+
+const HomeStack = createNativeStackNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const AccountScreen = props => {
+  return (
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Account Screen</Text>
+    </Layout>
+  );
+};
+
+const AccountStack = createNativeStackNavigator();
+
+const AccountStackScreen = () => {
+  return (
+    <AccountStack.Navigator
+      initialRouteName="Account"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <AccountStack.Group>
+        <AccountStack.Screen name="Account" component={AccountScreen} />
+      </AccountStack.Group>
+    </AccountStack.Navigator>
+  );
+};
+
+const BottomTabBar = ({navigation, state}) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    appearance="noIndicator"
+    onSelect={index => navigation.navigate(state.routeNames[index])}>
+    <BottomNavigationTab
+      title="Home"
+      icon={props => (
+        <MaterialIcons name="home" size={24} color={props.style.tintColor} />
+      )}
+    />
+    <BottomNavigationTab
+      title="Account"
+      icon={props => (
+        <MaterialIcons
+          name="account-circle"
+          size={24}
+          color={props.style.tintColor}
+        />
+      )}
+    />
+  </BottomNavigation>
+);
+
+const TabNavigator = createBottomTabNavigator();
+const TabNavigatorScreen = () => {
+  return (
+    <TabNavigator.Navigator tabBar={props => <BottomTabBar {...props} />}>
+      <TabNavigator.Screen name="HomeStackScreen" component={HomeStackScreen} />
+      <TabNavigator.Screen
+        name="AccountStackScreen"
+        component={AccountStackScreen}
+      />
+    </TabNavigator.Navigator>
+  );
+};
+
+export default TabNavigatorScreen;
